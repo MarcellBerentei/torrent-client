e0327a2ef46dcd423e5f30057738ff77e6169d54
Marcell Berentei <marcellberentei@gmail.com>
2026-09-17 11:12:19 +0200

    Implemented torrent init refactoring

--- main.c
+++ main.c
@@ -16,76 +16,26 @@
 int main(int argc, char *argv[]) {
     srand((unsigned)time(NULL));
 
-    // Generating a unique peer ID for every session
-    uint8_t peer_id[20];
-    generate_peer_id(peer_id);
-    //printf("Your Client Peer ID: %.*s\n\n", 20, peer_id);
+    Torrent torrent = {0};
+    char *torrent_path = (argc > 1) ? argv[1] : "test.torrent";
+    PeerConnection *peers = {0};
 
-    // I don't know what this means, but I wanted to try this out.
-    const char *torrent_path = (argc > 1) ? argv[1] : "test.torrent";
-    if (argc < 2) {
-        printf("No torrent file was entered, using default.\n\n");
-    }
+    if (torrent_init(&torrent, torrent_path) != 0) return 1;
 
-    // Opening the file, figureing out the size of it and then closeing the file.
-    char *file_buffer = NULL;
-    size_t file_size = 0;
-    if (load_torrent_file(torrent_path, &file_buffer, &file_size) != 0) {
-        printf("Failed to open torrent file: %s\n", torrent_path);
-        return 1;
-    }
-
-    //printf("Size of the file read is: %zu bytes.\n\n", file_size);
-
-    // Parsing the torrent file into a Bencode structure
-    Bencode *torrent_meta = parse_torrent_file(file_buffer);
-    if (torrent_meta == NULL) {
-        printf("Failed to parse Bencode\n\n");
-        free(file_buffer);
-        return 1;
-    }
-
-    //printf("Successfully parsed the torrent file!\n\n");
-
+    // From here, network.c should take over
     // Extracting the announce URLs from the torrent file. (From the announce-list, we skip the simple announce field)
     char **hosts = NULL;
     char **ports = NULL;
     int accepted_count = 0;
-    if (extract_announce_urls(torrent_meta, &hosts, &ports, &accepted_count) != 0) {
+    if (extract_announce_urls(torrent.torrent_meta, &hosts, &ports, &accepted_count) != 0) {
         printf("Failed to extract announce URLs\n\n");
-        free_bencode(torrent_meta);
-        free(file_buffer);
         return 1;
     }
 
-    //printf("Successfully extracted announce URLs!\n\n");
 
-    // Computing the info hash of the torrent file
-    uint8_t info_hash[20];
-    if (compute_info_hash(torrent_meta, info_hash) != 0) {
-        printf("Failed to compute torrent info hash\n\n");
-        free_bencode(torrent_meta);
-        free(file_buffer);
-        return 1;
-    }
-
-    // Computing the size of the bitfield
-    size_t bitfield_size = compute_bitfield_size(torrent_meta);
-    uint8_t *bitfield = calloc(bitfield_size, 1);
-    if (bitfield == NULL && bitfield_size > 0) {
-        printf("Failed to allocate bitfield\n");
-        free_bencode(torrent_meta);
-        free(file_buffer);
-        return 1;
-    }
-
-    // Start of the WinSock2 nightmare...
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         printf("WSAStartup failed\n");
-        free(bitfield);
-        free_bencode(torrent_meta);
-        free(file_buffer);
         return 1;
     }
 
@@ -97,13 +47,11 @@ int main(int argc, char *argv[]) {
     hints.ai_protocol = IPPROTO_UDP;
 
     // This talks to all the trackers and collects the peers from them.
+    // This should be called from network.c
     Peer *swarm = NULL;
     int swarm_count = 0;
-    if (collect_peers(NULL, &hints, info_hash, peer_id, accepted_count, hosts, ports, &swarm, &swarm_count) != 0) {
+    if (collect_peers(NULL, &hints, torrent.info_hash, torrent.peer_id, accepted_count, hosts, ports, &swarm, &swarm_count) != 0) {
         printf("Failed to collect peers from trackers\n");
-        free(bitfield);
-        free_bencode(torrent_meta);
-        free(file_buffer);
         for (int i = 0; i < accepted_count; i++) {
             free(hosts[i]);
             free(ports[i]);
@@ -130,12 +78,15 @@ int main(int argc, char *argv[]) {
         socket_poll_array[p].events = POLLOUT;
     }
 
+    // End of network.c, should be handed over to peer.c
 
     // === Talk to the peers === //
-    if (process_swarm(swarm, swarm_count, info_hash, peer_id) != 0) {
+    if (process_swarm(swarm, swarm_count, torrent.info_hash, torrent.peer_id) != 0) {
         printf("Error processing swarm\n");
     }
 
+/*
+    // This can stay here
     // === Cleanup === //
     cleanup_swarm(swarm, swarm_count);
     free(swarm);
@@ -149,5 +100,6 @@ int main(int argc, char *argv[]) {
     free(ports);
     free(bitfield);
     WSACleanup();
+    */
     return 0;
 }
--- torrent.h
+++ torrent.h
@@ -3,7 +3,20 @@
 #include <stdint.h>
 #include "bencode.h"
 
-int load_torrent_file(const char *path, char **buffer, size_t *size);
+int load_torrent_file(char *path, char **buffer, size_t *size);
 Bencode *parse_torrent_file(char *buffer);
 int compute_info_hash(Bencode *b, uint8_t hash_out[20]);
 size_t compute_bitfield_size(Bencode *b);
+
+typedef struct {
+    uint8_t peer_id[20];
+    uint8_t info_hash[20];
+    uint8_t *bitfield;
+    char *torrent_path;
+    char *file_buffer;
+    size_t file_size;    
+    size_t bitfield_size;
+    Bencode *torrent_meta;    
+} Torrent;
+
+int torrent_init(Torrent *torrent, char *torrent_path);
